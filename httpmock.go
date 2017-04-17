2fe0df956751695cfd1e18825b342dc9269eea96
Eder Ruiz Maria <drrzmr@gmail.com>
2017-04-17 20:40:37 -0300

    http: Add support to add get server handler

--- httpmock.go
+++ httpmock.go
@@ -8,13 +8,18 @@ import (
 	"testing"
 
 	"github.com/fatih/color"
+	"github.com/stretchr/testify/require"
 )
 
 // MockServer wrapper for httptest.Server
 type MockServer struct {
 	server *httptest.Server
 	t      *testing.T
+	get    map[string]GetHandler
 }
+
+// GetHandler Server GET handler
+type GetHandler func(w http.ResponseWriter, r *http.Request)
 
 func dumpServerRequest(r *http.Request, printBody bool) error {
 	dump, err := httputil.DumpRequest(r, printBody)
@@ -41,21 +46,41 @@ func ClientDumpResponse(r *http.Response, printBody bool) error {
 	return nil
 }
 
+// ServerDumpRequest dump request from client
+func ServerDumpRequest(r *http.Request, printBody bool) error {
+	return dumpServerRequest(r, printBody)
+}
+
 // New create a MockServer instance
 func New(t *testing.T, dumpRequest bool, dumpBody bool) MockServer {
 
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	ms := MockServer{
+		t:   t,
+		get: map[string]GetHandler{},
+	}
+
+	ms.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		if true == dumpRequest {
 			if err := dumpServerRequest(r, dumpBody); err != nil {
 				http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 			}
+		}
 
+		switch r.Method {
+		case "GET":
+			callback, ok := ms.get[r.URL.Path]
+			require.Equal(ms.t, ok, true, fmt.Sprintf("could not found GET handler for: %s", r.URL.Path))
+			callback(w, r)
+		default:
+			require.Fail(t, fmt.Sprintf("http method not found: %s", r.Method))
 		}
 	}))
 
-	return MockServer{
-		server: server,
-		t:      t,
-	}
+	return ms
+}
+
+// AddGetHandler add GET handler to specific path
+func (ms *MockServer) AddGetHandler(path string, handler GetHandler) {
+	ms.get[path] = handler
 }
--- httpmock_test.go
+++ httpmock_test.go
@@ -23,3 +23,16 @@ func TestGet(t *testing.T) {
 
 	ms.server.Close()
 }
+
+func TestAddGetHandler(t *testing.T) {
+	ms := New(t, false, false)
+	ms.AddGetHandler("/test", func(w http.ResponseWriter, r *http.Request) {
+		ServerDumpRequest(r, true)
+	})
+
+	resp, err := http.Get(ms.server.URL + "/test")
+	require.NoError(t, err, "server are not fine :(")
+	require.NoError(t, ClientDumpResponse(resp, true), "response are not ok :(")
+
+	ms.server.Close()
+}
